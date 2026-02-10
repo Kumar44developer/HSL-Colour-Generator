2aa289fb9b8f1fdcc1ac82a019093ccce30f2e01
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-02-10 23:00:53 +0530

    Selected lightness input element

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.addEventListener("DOMContentLoaded",function(){
     const hueInput = document.getElementById("hue");
     const saturationInput = document.getElementById("saturation");
+    const lightnessInput = document.getElementById("lightness");
