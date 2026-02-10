e8c9d8bc98151ea0c505ae3ad81b3262d6373fd7
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-02-10 23:01:19 +0530

    Selected hue value display element

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ document.addEventListener("DOMContentLoaded",function(){
     const hueInput = document.getElementById("hue");
     const saturationInput = document.getElementById("saturation");
     const lightnessInput = document.getElementById("lightness");
+
+    const hueValueSpan = document.getElementById("hueValue");
