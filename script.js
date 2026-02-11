cde55f2b155ed327e0f05a5fd386cfd8edabd8b0
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-02-11 20:14:56 +0530

    Added error handling for clipboard copy

--- script.js
+++ script.js
@@ -36,3 +36,4 @@ document.addEventListener("DOMContentLoaded",function(){
         const textToCopy = `hsl(${hueInput.value}, ${saturationInput.value}%, ${lightnessInput.value}%)`;
         navigator.clipboard.writeText(textToCopy).then(function(){
             alert("The color is copied to Clipboard")
+        }).catch(function(err){
